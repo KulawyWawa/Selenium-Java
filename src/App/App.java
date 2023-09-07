2ab4a476dc13f8e5c3c4eb9ec4403031963152c6
KulawyWawa <123329748+KulawyWawa@users.noreply.github.com>
2023-09-07 20:21:37 +0200

    selenium
    // Selenium / /

---
+++ src/App/App.java
@@ -0,0 +1,15 @@
+package App;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ChromeDriver ChromeDriver = new ChromeDriver();
+		
+	}
+}
